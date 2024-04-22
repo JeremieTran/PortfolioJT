39616a3a57ba5a78e34d863ef357f5343dc7f36a
JeremieBTSSIO49380 <conundrum@hotmail.fr>
2024-04-22 09:23:34 +0200

    update

--- mission2.php
+++ mission2.php
@@ -86,7 +86,20 @@
                                 <h4>PhpMyAdmin </h4>
                             </div>
                         </div>
-                        <!-- Fin Bloc Annexe -->   
+                        <!-- Fin Bloc Annexe -->  
+                        <!-- Début Bloc Annexe -->
+                        <div class="d-flex mb-3">
+                            <a href="img/InstallMysql.pdf">
+                            <div class="btn-icon bg-primary mr-4">
+                                <i class="fa fa-2x fa-paperclip text-white"></i>
+                            </div>
+                            </a>
+                            <div class="mt-n1">
+                                <br/>
+                                <h4>PhpMyAdmin </h4>
+                            </div>
+                        </div>
+                        <!-- Fin Bloc Annexe -->    
                     </div>
 
                     
