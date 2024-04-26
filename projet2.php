9fb9773dd1b889829e5e1237b6e79d650c8b5daa
JeremieBTSSIO49380 <conundrum@hotmail.fr>
2024-04-26 16:42:42 +0200

    add Diagramme

--- projet2.php
+++ projet2.php
@@ -104,6 +104,19 @@
                             </div>
                         </div>
                         <!-- Fin Bloc Annexe -->   
+                        <!-- Début Bloc Annexe -->
+                        <div class="d-flex mb-3">
+                            <a href="img/sequenceveille.png">
+                            <div class="btn-icon bg-primary mr-4">
+                                <i class="fa fa-2x fa-paperclip text-white"></i>
+                            </div>
+                            </a>
+                            <div class="mt-n1">
+                                <br/>
+                                <h4>Diagramme de classe</h4>
+                            </div>
+                        </div>
+                        <!-- Fin Bloc Annexe -->  
                     </div>
 
                     
